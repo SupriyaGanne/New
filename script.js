c98ed26ed42e88702f56b126edf2eaefe02405ce
Ganne Supriya <gannesupriya@gmail.com>
2023-05-29 08:18:29 +0530

    third commit

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function convertDegree() {
   var userinput = document.getElementById("temp").value;
+
   var userselction = document.getElementById("u").value;
+
   var convertas = 0;
+  
   var output= document.getElementById("resultcontainer");
 
   if ( userselction === "cel") {
-    // Convert Celsius to Fahrenheit
     convertas = (userinput * 9 / 5) + 32;
     output.innerHTML =  " = " + convertas+ " °F";
   } else if ( userselction === "fah") {
-    // Convert Fahrenheit to Celsius
     convertas = (userinput - 32) * 5 / 9;
     output.innerHTML = " = " + convertas + " °C";
   } else if ( userselction === "kel") {
-    // Convert Kelvin to Celsius
     convertas = userinput - 273.15;
     output.innerHTML =  " = " + convertas + " °C";
   }
+  
 }
  
